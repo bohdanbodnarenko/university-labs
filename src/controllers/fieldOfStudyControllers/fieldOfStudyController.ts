92e434470171733793b665f4177ff85d5942d51c
Bogdan <bodya.bodnarenko@gmail.com>
2019-12-15 16:29:34 +0200

    added create fos route

--- src/controllers/fieldOfStudyControllers/fieldOfStudyController.ts
+++ src/controllers/fieldOfStudyControllers/fieldOfStudyController.ts
@@ -4,3 +4,15 @@ import { FieldOfStudy } from "../../entity/FieldOfStudy";
 export const getFieldsOfStudy = async(req:Request,res:Response) => {
      res.json(await FieldOfStudy.find())
 }
+
+export const createFieldOfStudy = async(req:Request,res:Response) => {
+    const {name} = req.body;
+    const names = await FieldOfStudy.find({select:['name']})
+    
+    if (names.includes(name)) {
+        return res.status(404).json({error:'This field of study already exists'})
+    }
+
+    const newFieldOfStudy = await FieldOfStudy.create({name}).save()
+    res.json(200).json(newFieldOfStudy)
+}
--- src/routes/fieldOfStudyRoutes.ts
+++ src/routes/fieldOfStudyRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getFieldsOfStudy } from "../controllers/fieldOfStudyControllers/fieldOfStudyController";
+import { getFieldsOfStudy, createFieldOfStudy } from "../controllers/fieldOfStudyControllers/fieldOfStudyController";
 
 export const fieldOfStudyRouter = Router();
 
@@ -23,4 +23,20 @@ export const fieldOfStudyRouter = Router();
     }
  ]
  */
-fieldOfStudyRouter.get('/all',getFieldsOfStudy)
+fieldOfStudyRouter.get('/all', getFieldsOfStudy)
+
+/**
+ * @api {post} /fos Create a new field of study
+ * @apiGroup fos
+ * @apiParam {String} name Field of study's name
+ * @apiSuccess {FieldOfStudy} Field of study
+ * @apiSuccess {Number} id Field of study id
+ * @apiSuccess {String} name Field of study name
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ {
+        "id": 1,
+        "name": "blabla",
+    }
+ */
+fieldOfStudyRouter.post('/', createFieldOfStudy)
